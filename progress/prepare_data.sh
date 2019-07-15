8d0cffd31de073375c7d40249f6cd1334e7e0b36
jacky <jacky84228@hotmail.com>
2019-07-15 14:49:33 +0800

    modify prepare_data

--- progress/prepare_data.sh
+++ progress/prepare_data.sh
@@ -4,6 +4,8 @@
 audio_dir=vocal_data
 
 numjob=4
+# level : gener, utt, singer (spk2utt fill will be different)
+
 for level in utt ; do
   for part in train_clean test_clean ; do
     local/vocaldata_prep_$level.sh $audio_dir/$part data/paper/$part\_$level || exit 1;
@@ -14,18 +16,3 @@ for level in utt ; do
   done
 done
 
-if  false ; then 
-  for level in genre ; do
-    for part in train_clean test_clean ; do
-      if [ ! -d $audio_dir/genre_$part ]; then
-        echo "divide $audio_dir/genre_$part"
-        python pyutils/divide_genre_set.py $part genre_$part
-      fi
-      local/vocaldata_prep_singer.sh $audio_dir/genre_$part data/paper/$part\_$level || exit 1;
-      steps/make_mfcc.sh --cmd "$train_cmd" --nj $numjob \
-        data/paper/$part\_$level exp/make_mfcc/paper/$part\_$level mfcc/paper/$part\_$level || exit 1;
-      steps/compute_cmvn_stats.sh \
-        data/paper/$part\_$level exp/make_mfcc/paper/$part\_$level mfcc/paper/$part\_$level || exit 1;
-    done
-  done
-fi
